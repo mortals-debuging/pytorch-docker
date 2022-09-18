17f6875ddbc5c9d460f1d21f3a7bd182e4f0f5d7
mortals-debuging <460724359@qq.com>
2022-09-18 21:16:32 +0800

    add ssh

--- sshstart.sh
+++ sshstart.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-if (cat /etc/ssh/ssh_config|grep "^    PermitRootLogin yes" >NP); then cd .; else  sed -i "$ a \    PermitRootLogin yes" /etc/ssh/ssh_config; fi
-if (cat /etc/ssh/ssh_config|grep "^    UsePAM no">NP); then cd .; else  sed -i "$ a \    UsePAM no" /etc/ssh/ssh_config; fi
+if (cat /etc/ssh/ssh_config|grep "^    PermitRootLogin yes" >NP); then cd .; else  sed -i "$ a \    PermitRootLogin yes" /etc/ssh/sshd_config; fi
+if (cat /etc/ssh/ssh_config|grep "^    UsePAM no">NP); then cd .; else  sed -i "$ a \    UsePAM no" /etc/ssh/sshd_config; fi
 if (cat ~/.bashrc|grep "^service ssh start">NP); then
   cd .;
   else
     sed -i "$ a \service ssh start" ~/.bashrc
     service ssh start
-    echo "SSH service will be started as container start."
     echo -e "\e[1;7;31mPlease set your password! \nPlease set your password! \nPlease set your password! \e[0m";
     passwd
 fi
