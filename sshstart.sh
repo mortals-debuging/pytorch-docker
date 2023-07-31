173c2475820df7d451778db98a87b8886e999ef4
mortals-debuging <460724359@qq.com>
2023-07-31 20:46:48 +0800

    fix a reg bug

--- sshstart.sh
+++ sshstart.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 ssh_command="service ssh start"
-profile_file="/etc/profile"
-root_profile_file="/root/.profile"
+profile_file="/root/.bashrc"
 ssh_config_file="/etc/ssh/ssh_config"
 use_pam_option="UsePAM no"
 
 # Check if the "service ssh start" command is present in /etc/profile or /root/.profile
-if grep -q "^$ssh_command" "$profile_file" || grep -q "$ssh_command" "$root_profile_file"; then
+if grep -q "^$ssh_command" "$profile_file"; then
     echo "SSH is set to start automatically."
 else
     echo "Adding SSH startup command to /etc/profile."
     echo "$ssh_command" >> "$profile_file"
+    service ssh start
     printf "\e[1;31;7mPlease set your password! \nPlease set your password! \nPlease set your password! \e[0m\n"
     read -p "Do you want to set a password now? (y/n) " choice
     if [[ $choice == "y" || $choice == "Y" || $choice == "" ]]; then
